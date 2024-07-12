0eda21c6bfe0a2fa074c7e638d4bb685408608cb
Alex Prostamo <alexprostamo@Air-di-Alex.fritz.box>
2024-07-12 14:38:43 +0200

    Update commit

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import { View, Text, LogBox } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+export default function layout() {
+  LogBox.ignoreLogs((''))
+  return (
+    <Stack
+    screenOptions={{
+        headerShown: false
+    }}
+    >
+      <Stack.Screen name='exercises' options={{presentation: 'fullScreenModal'}}/>
+      <Stack.Screen name='exerciseDetails' options={{presentation: 'modal'}}/>
+    </Stack>
+  )
+}
